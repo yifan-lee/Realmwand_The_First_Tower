75657b3b3e1483d3e1f5b546223b1fb8def50418
yifan-lee <ivanlee142857@gmail.com>
2026-07-30 19:54:20 -0400

    add eventbus and floor manager

---
+++ scripts/autoload/event_bus.gd
@@ -0,0 +1,6 @@
+extends Node
+
+signal floor_change_requested(
+	target_floor_id: StringName,
+	target_spawn_id: StringName
+)
---
+++ scripts/floors/floor_manager.gd
@@ -0,0 +1,6 @@
+class_name FloorManager
+extends Node
+
+@export_group("Startup")
+@export var starting_floor_id: StringName = &"floor_1"
+@export var starting_spawn_id: StringName = &"FromStart"
