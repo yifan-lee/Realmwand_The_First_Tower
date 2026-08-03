3499d972570b5a8d2cde5916a372d335cb58cf60
yifan-lee <ivanlee142857@gmail.com>
2026-08-02 23:06:33 -0400

    add 3 switch logic

--- scripts/floors/floor_1.gd
+++ scripts/floors/floor_1.gd
@@ -1,89 +1,76 @@
 extends Floor
 
 
-class SwitchTerrainRule:
-	extends RefCounted
-
-	var switch_id: StringName
-	var wall_cells: Array[Vector2i]
-	var wall_snapshots: Array[Floor.TileCellSnapshot] = []
-
-	func _init(
-		target_switch_id: StringName,
-		target_wall_cells: Array[Vector2i]
-	) -> void:
-		switch_id = target_switch_id
-		wall_cells = target_wall_cells
-
-
 @onready var wall_layer: TileMapLayer = %WallLayer
 
-var switch_rules: Array[SwitchTerrainRule] = []
+@onready var wall_switch: FloorSwitch = $Interactables/WallSwitch
+@onready var switch_red: FloorSwitch = $Interactables/SwitchRed
+@onready var switch_blue: FloorSwitch = $Interactables/SwitchBlue
+@onready var switch_yellow: FloorSwitch = $Interactables/SwitchYellow
+@onready var up_stair: Area2D = $Interactables/UpStair
+
+var wall_passage_snapshots: Array[Floor.TileCellSnapshot] = []
+var stair_wall_snapshots: Array[Floor.TileCellSnapshot] = []
 
 
 func _ready() -> void:
-	_create_switch_rules()
 	_cache_switch_terrain()
 	_apply_initial_switch_states()
 
 
-func _create_switch_rules() -> void:
-	switch_rules = [
-		SwitchTerrainRule.new(
-			&"wall_passage",
-			[
-				Vector2i(1, -6),
-				Vector2i(0, -6),
-                Vector2i(-1, -6),
-			]
-		),
-	]
-
-
 func _cache_switch_terrain() -> void:
-	for rule: SwitchTerrainRule in switch_rules:
-		rule.wall_snapshots = capture_tile_cells(
-			wall_layer,
-			rule.wall_cells
-		)
+	wall_passage_snapshots = capture_tile_cells(
+		wall_layer,
+		[
+			Vector2i(1, -6),
+			Vector2i(0, -6),
+			Vector2i(-1, -6),
+		]
+	)
+	stair_wall_snapshots = capture_tile_cells(
+		wall_layer,
+		[
+			Vector2i(0, -9),
+		]
+	)
 
 
 func _apply_initial_switch_states() -> void:
-	for child: Node in interactables.get_children():
-		var floor_switch := child as FloorSwitch
-
-		if floor_switch == null:
-			continue
-
-		_apply_switch_state(
-			floor_switch.switch_id,
-			floor_switch.is_active
-		)
+	_update_wall_passage()
+	_update_colored_switches()
 
 
 func _on_floor_switch_state_changed(
 	switch_id: StringName,
-	is_active: bool
+	_is_active: bool
 ) -> void:
-	_apply_switch_state(switch_id, is_active)
+	match switch_id:
+		&"wall_passage":
+			_update_wall_passage()
+		&"switch_red", &"switch_blue", &"switch_yellow":
+			_update_colored_switches()
 
 
-func _apply_switch_state(
-	switch_id: StringName,
-	is_active: bool
-) -> void:
-	for rule: SwitchTerrainRule in switch_rules:
-		if rule.switch_id != switch_id:
-			continue
-
-		set_tile_cells_removed(
-			wall_layer,
-			rule.wall_snapshots,
-			is_active
-		)
-		return
-
-	push_warning(
-		"Floor '%s' has no terrain rule for switch '%s'."
-		% [floor_id, switch_id]
+func _update_wall_passage() -> void:
+	set_tile_cells_removed(
+		wall_layer,
+		wall_passage_snapshots,
+		wall_switch.is_active
 	)
+
+
+func _update_colored_switches() -> void:
+	var all_active := (
+		switch_red.is_active
+		and switch_blue.is_active
+		and switch_yellow.is_active
+	)
+
+	set_tile_cells_removed(
+		wall_layer,
+		stair_wall_snapshots,
+		all_active
+	)
+	up_stair.visible = all_active
+	up_stair.monitoring = all_active
+	up_stair.monitorable = all_active
